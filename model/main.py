4902664c1571796db5de1cb6c2ea19f312cc949a
Sasha <q.dev.null@gmail.com>
2021-01-28 12:57:28 +0100

    Extract the features and the edgelist

--- model/main.py
+++ model/main.py
@@ -9,14 +9,18 @@ def main():
         inplace=True,
     )
 
+    x = features.drop(columns=["node", "target"]).values
+    y = features["target"].astype("category").cat.codes.values
+
     iton = features["node"].to_list()
     ntoi = {n: i for i, n in enumerate(iton)}
-    print(ntoi)
 
     df = pd.read_table("data/cora/cora.cites", names=["source", "target"])
     df["source"] = df["source"].map(ntoi)
     df["target"] = df["target"].map(ntoi)
-    print(df)
+
+    edge_list = df.values
+    return x, edge_list, y
 
 
 if __name__ == '__main__':
