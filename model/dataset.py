eb86bbe75b7705bd75fa3887a1d966a56938d350
Sasha <q.dev.null@gmail.com>
2021-01-28 14:43:46 +0100

    Separate the dataset loaders

---
+++ model/dataset.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import pathlib
+
+
+def load_cora(path="data/cora"):
+    path = pathlib.Path(path)
+
+    features = pd.read_table(path / "cora.content", header=None)
+    features.rename(columns={
+        features.columns[0]: "node",
+        features.columns[-1]: "target"},
+        inplace=True,
+    )
+
+    x = features.drop(columns=["node", "target"]).values
+    y = features["target"].astype("category").cat.codes.values
+
+    iton = features["node"].to_list()
+    ntoi = {n: i for i, n in enumerate(iton)}
+
+    df = pd.read_table(path / "cora.cites", names=["source", "target"])
+    df["source"] = df["source"].map(ntoi)
+    df["target"] = df["target"].map(ntoi)
+
+    edge_list = df.values
+    return x, edge_list, y
--- model/main.py
+++ model/main.py
@@ -1,26 +1,9 @@
-import pandas as pd
+from model.dataset import load_cora
 
 
 def main():
-    features = pd.read_table("data/cora/cora.content", header=None)
-    features.rename(columns={
-        features.columns[0]: "node",
-        features.columns[-1]: "target"},
-        inplace=True,
-    )
-
-    x = features.drop(columns=["node", "target"]).values
-    y = features["target"].astype("category").cat.codes.values
-
-    iton = features["node"].to_list()
-    ntoi = {n: i for i, n in enumerate(iton)}
-
-    df = pd.read_table("data/cora/cora.cites", names=["source", "target"])
-    df["source"] = df["source"].map(ntoi)
-    df["target"] = df["target"].map(ntoi)
-
-    edge_list = df.values
-    return x, edge_list, y
+    x, edge_list, y = load_cora()
+    print(x, edge_list, y)
 
 
 if __name__ == '__main__':
