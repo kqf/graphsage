9c835af055baa2c01d4a97f3ab84a0fa4708a665
Sasha <q.dev.null@gmail.com>
2021-01-29 20:11:43 +0100

    Fix the main training method

--- model/main.py
+++ model/main.py
@@ -1,9 +1,15 @@
-from model.dataset import load_cora
+from model.dataset import load_cora, sampling_iterator
+from model.layers import GraphSAGE
 
 
 def main():
-    x, edge_list, y = load_cora()
-    print(x, edge_list, y)
+    dataset = load_cora()
+
+    model = GraphSAGE()
+
+    batches = sampling_iterator(dataset, batch_size=64)
+    for batch in batches:
+        model.forward(*batches)
 
 
 if __name__ == '__main__':
