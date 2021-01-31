ff6c369503d0cd7dba12b66a823d817f0579d844
Sasha <q.dev.null@gmail.com>
2021-01-31 13:09:53 +0100

    Add the model definition

---
+++ model/model.py
@@ -0,0 +1,37 @@
+import torch
+import skorch
+
+
+from model.dataset import GraphLoader
+
+
+def init(w):
+    if w.dim() < 2:
+        return w
+    return torch.nn.init.xavier_uniform_(w)
+
+
+def build_model(max_epochs=2, logdir=".tmp/", train_split=None):
+    model = skorch.NeuralNetClassifier(
+        # ResUNet,
+        module__pretrained=False,
+        criterion=torch.nn.CrossEntropyLoss,
+        criterion__padding=0,
+        batch_size=32,
+        max_epochs=max_epochs,
+        # optimizer__momentum=0.9,
+        iterator_train=GraphLoader,
+        iterator_train__shuffle=True,
+        iterator_train__num_workers=4,
+        iterator_valid=GraphLoader,
+        iterator_valid__shuffle=False,
+        iterator_valid__num_workers=4,
+        train_split=train_split,
+        callbacks=[
+            skorch.callbacks.ProgressBar(),
+            skorch.callbacks.Initializer("*", init),
+        ],
+        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+    )
+
+    return model
