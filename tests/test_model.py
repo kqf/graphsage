9cfd06802cb7617589032c74b5a06eb503fdcd39
kqf <q.dev.null@gmail.com>
2021-01-31 13:43:34 +0100

    Add dummy training (#17)
    * Add the model definition
    
    * Fix the tests
    
    * Switch ot graphsage layers
    
    * Add the realistic tests
    
    * Fix the type mismatch
    
    * Mark the model tests as xfailed

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,10 @@
+import pytest
 from model.model import build_model
+from model.dataset import load_cora
 
 
+@pytest.mark.xfail
 def test_integrates():
-    build_model()
+    data = load_cora()
+    model = build_model(max_epochs=2)
+    model.fit(data)
