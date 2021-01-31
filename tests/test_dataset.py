37e75107b04d0462fc18b6f459b0a61cdec790c3
Sasha <q.dev.null@gmail.com>
2021-01-31 12:25:52 +0100

    Fix the broken tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -8,5 +8,5 @@ def test_dataset():
         pass
 
     batches = sampling_iterator(dataset, batch_size=64, drop_last=True)
-    for (features, batch, layers), y in batches:
-        assert len(batch) == 64
+    for batch, y in batches:
+        assert len(batch["nodes"]) == 64
