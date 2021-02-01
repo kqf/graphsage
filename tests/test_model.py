392790cd8af054505a5b66547cdaca74daf9b983
kqf <q.dev.null@gmail.com>
2021-02-01 10:37:05 +0100

    Fix lentgh consistency (#19)
    * Uncomment tests
    
    * Hack the NeuralNet classifier
    
    * Remove the comments from tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,7 @@
-import pytest
 from model.model import build_model
 from model.dataset import load_cora
 
 
-@pytest.mark.xfail("Length consistency")
 def test_integrates():
     data = load_cora()
     model = build_model(max_epochs=2)
