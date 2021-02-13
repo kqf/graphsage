b8fafe52b7d53f3dd5431b66862b926d10590263
Sasha <q.dev.null@gmail.com>
2021-02-13 09:25:26 +0100

    Fix the loss calculation

--- graphsage/model.py
+++ graphsage/model.py
@@ -42,7 +42,8 @@ class UnsupervisedGraphNet(GraphNet):
         if isinstance(self.criterion_, torch.nn.Module):
             self.criterion_.train(training)
 
-        return self.criterion_(y_pred, y_pred, y_pred)
+        anchor, positive, negative = y_pred.split(y_pred.shape[0] // 3, dim=0)
+        return self.criterion_(anchor, positive, negative)
 
 
 def build_model(max_epochs=2, logdir=".tmp/", train_split=None):
