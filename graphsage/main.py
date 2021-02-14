32f0812441209b0876b22fd6931d2b4f943a7f21
Sasha <q.dev.null@gmail.com>
2021-02-14 06:53:12 +0100

    Reduce the batch size

--- graphsage/main.py
+++ graphsage/main.py
@@ -4,7 +4,9 @@ from graphsage.model import build_model
 
 def main():
     data = load_cora()
-    model = build_model(max_epochs=20)
+    model = build_model(
+        max_epochs=20
+    )
     model.fit(data)
 
 
