2164663910e5778d8390555dd44970d2ec6af3e4
kqf <q.dev.null@gmail.com>
2021-03-07 07:30:00 +0100

    Add tests for local indexing (#41)
    * Fix the dataset preparation
    
    * Prepare the inputs for the tests
    
    * Fix the iterator

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -1,3 +1,7 @@
+import pytest
+import numpy as np
+import pandas as pd
+
 from graphsage.dataset import GraphLoader, NegativeGraphLoader
 
 
@@ -21,3 +25,38 @@ def test_negative_sampling(data):
         assert len(batch["nodes"]) == 64 * 3
         assert "layers" in batch
         assert "features" in batch
+
+
+@pytest.fixture
+def batch_layers():
+    anchors = np.array([1, 2, 3, 4])
+    layers = [
+        (
+            anchors,
+            pd.DataFrame({
+                "source": [1, 2, 3, 4],
+                "target": [2, 3, 4, 5],
+            })
+        )
+    ]
+    layers.append(
+        (
+            np.array([1, 2, 3, 4, 5, 6]),
+            pd.DataFrame({
+                "source": [2, 3, 4, 5],
+                "target": [3, 4, 5, 6],
+            })
+        )
+    )
+    return anchors, layers[::-1]
+
+
+def test_local_mapping(batch_layers):
+    batch, layers = batch_layers
+    all_nodes, lbatch, llayers = GraphLoader.to_local(batch, layers)
+
+    # Global mapping
+    np.testing.assert_equal(all_nodes, np.array([1, 2, 3, 4, 5, 6]))
+
+    # The local mapping
+    np.testing.assert_equal(lbatch, np.array([0, 1, 2, 3]))
